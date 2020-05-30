dac77b11ce733891da9ce9e68577cf9315a87418
ennnnny <kuye1130@gmail.com>
2020-05-30 16:55:18 +0800

    新增luci-theme-argon主题 关闭IPV6

--- diy.sh
+++ diy.sh
@@ -8,9 +8,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.18.1/g' openwrt/package/base-files/files/bin/config_generate
 # Install ServerChan
-git clone https://github.com/tty228/luci-app-serverchan openwrt/package/lean/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git openwrt/package/lean/luci-app-serverchan
 # Install passwall
 cp -r lienol/luci-app-passwall openwrt/package/lean/luci-app-passwall
+# Install luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
 # Sync Lede package
 git clone https://github.com/coolsnowwolf/lede.git lede
 # Install cifs-mount
@@ -18,5 +20,5 @@ cp -r lede/package/lean/luci-app-cifs-mount openwrt/package/lean/luci-app-cifs-m
 # Install ttyd
 cp -r lede/package/lean/luci-app-ttyd openwrt/package/lean/luci-app-ttyd
 # Install OpenClash
-git clone https://github.com/vernesong/OpenClash.git OpenClash
+git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
 cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
