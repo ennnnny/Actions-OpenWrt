f4a73c1fa812f623cdbf63fa476d22fd42c58fd4
ennnnny <kuye1130@gmail.com>
2020-07-04 00:57:53 +0800

    update

--- diy.sh
+++ diy.sh
@@ -19,11 +19,11 @@ cp -r lede/package/lean/luci-app-ttyd openwrt/package/lean/luci-app-ttyd
 git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
 cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
 ## integration clash core 实现编译更新后直接可用，不用手动下载clash内核
-#curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
-#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-#chmod +x /tmp/clash >/dev/null 2>&1
-#mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
-#mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
-#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
+mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 #Backup OpenClash cofig
 echo '/etc/openclash/' >> openwrt/package/base-files/files/etc/sysupgrade.conf
