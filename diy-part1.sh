119b963eca68cce889f921c86f300e8f921fd6a7
ennnnny <kuye1130@gmail.com>
2021-02-28 01:06:39 +0800

    change to lede

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' openwrt/feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' openwrt/feeds.conf.default
 
 # Update Luci
-sed -i 's/openwrt-luci.git;17.01/openwrt-luci.git;19.07/g' openwrt/feeds.conf.default
+#sed -i 's/openwrt-luci.git;17.01/openwrt-luci.git;19.07/g' openwrt/feeds.conf.default
 ## Add Hello World
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' openwrt/feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.18.1/g' openwrt/package/base-files/files/bin/config_generate
 # Install ServerChan
-git clone https://github.com/tty228/luci-app-serverchan.git openwrt/package/lean/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan.git openwrt/package/lean/luci-app-serverchan
 # Add Other Theme
-git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
+rm -rf openwrt/package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
 #git clone https://github.com/garypang13/luci-theme-edge.git openwrt/package/lean/luci-theme-edge
 ## Install passwall
 #cp -r lienol/luci-app-passwall openwrt/package/lean/luci-app-passwall
@@ -24,14 +25,14 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean
 ## Install ttyd
 #cp -r lede/package/lean/luci-app-ttyd openwrt/package/lean/luci-app-ttyd
 # Install OpenClash
-git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
-cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
+#git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
+#cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
 ## integration clash core 实现编译更新后直接可用，不用手动下载clash内核
-curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
-mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+#curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+#chmod +x /tmp/clash >/dev/null 2>&1
+#mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
+#mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 #Backup OpenClash cofig
-echo '/etc/openclash/' >> openwrt/package/base-files/files/etc/sysupgrade.conf
+#echo '/etc/openclash/' >> openwrt/package/base-files/files/etc/sysupgrade.conf
