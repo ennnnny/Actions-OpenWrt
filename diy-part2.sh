bece267249bf6dd83390bef3dca4e7bf99487c67
ennnnny <kuye1130@gmail.com>
2021-02-28 22:47:39 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.18.1/g' openwrt/package/base-files/files/bin/config_generate
 # Install ServerChan
-#git clone https://github.com/tty228/luci-app-serverchan.git openwrt/package/lean/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git openwrt/package/lean/luci-app-serverchan
 # Add Other Theme
 rm -rf openwrt/package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
@@ -25,14 +25,16 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/pac
 ## Install ttyd
 #cp -r lede/package/lean/luci-app-ttyd openwrt/package/lean/luci-app-ttyd
 # Install OpenClash
-#git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
-#cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
+git clone -b master https://github.com/vernesong/OpenClash.git OpenClash
+cp -r OpenClash/luci-app-openclash openwrt/package/lean/luci-app-openclash
 ## integration clash core 实现编译更新后直接可用，不用手动下载clash内核
-#curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
-#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-#chmod +x /tmp/clash >/dev/null 2>&1
-#mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
-#mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
-#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p openwrt/package/lean/luci-app-openclash/files/etc/openclash/core
+mv /tmp/clash openwrt/package/lean/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 #Backup OpenClash cofig
-#echo '/etc/openclash/' >> openwrt/package/base-files/files/etc/sysupgrade.conf
+echo '/etc/openclash/' >> openwrt/package/base-files/files/etc/sysupgrade.conf
+#install JD-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git openwrt/package/lean/luci-app-jd-dailybonus
